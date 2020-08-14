e62d001539becafeac070ab721808fec23a4bf49
Peeyush <peeyush.dmajgawali.eee17@itbhu.ac.in>
2020-08-14 19:00:55 +0530

    :-)

---
+++ src/ReverseLettersAndWords.java
@@ -0,0 +1,23 @@
+public class ReverseLettersAndWords {
+
+    public static String foo(String s){
+        StringBuilder stringBuilder = new StringBuilder(s);
+        return stringBuilder.reverse().toString();
+    }
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        String[] words = s.split(" ");
+
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder revWord = new StringBuilder(word);
+            revWord.reverse();
+            ans.append(revWord).append(" ");
+        }
+
+        System.out.println(ans.toString());
+        System.out.println(foo(s));
+    }
+}
